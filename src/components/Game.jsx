0f8bf04c998741fbfa853253bcead28afe8b55dd
CJ Rasos <cj.rasos30@gmail.com>
2024-11-12 18:09:51 -0800

    delete math

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,7 +4,6 @@ const Game = () => {
     return (
         <div className="flex items-center justify-center h-screen bg-[#D5F3FE]">
             <h2 className="text-3xl font-bold mb-20">Game Section</h2>
-            <h3 className="text-2xl text-green-600 mx-20" >Math</h3>
         </div>
     );
 };

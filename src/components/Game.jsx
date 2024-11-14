581b9b7f757cb89c74c06a8387c561bd9ca928d9
CJ Rasos <cj.rasos30@gmail.com>
2024-11-13 21:24:02 -0800

    Add random paragraph

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ const Game = () => {
     return (
         <div className="flex items-center justify-center h-screen bg-[#D5F3FE]">
             <h2 className="text-3xl font-bold mb-20">Game Section: Math</h2>
+            <p className="text-lg font-semibold">This is the game section for math.</p>
         </div>
     );
 };
